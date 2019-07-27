c537df947fa9b9c4d757a34f5ff783613623551a
rinoshinme <rinoshinme@163.com>
2019-07-27 13:26:00 +0800

    add mac osx support for export saved model

--- export_tfserving/export_savedmodel.py
+++ export_tfserving/export_savedmodel.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import platform
 import tensorflow as tf
 import base64
 import numpy as np
@@ -20,6 +21,8 @@ from export_tfserving.violence_model import ViolenceModelResNetV1L101, InputType
 """Builds a SavedModel which can be used for deployment with
 gcloud ml-engine, tensorflow-serving, ...
 """
+
+os_type = platform.system()
 
 
 def test_model(model, image_path, sess=None):
@@ -59,13 +62,21 @@ if __name__ == "__main__":
     model = ViolenceModelResNetV1L101(num_classes=9)
 
     # parameters
-    export_base_path = r'D:/projects/slim_finetune/export_tfserving/serve_models9/'
+    if os_type == 'Darwin':
+        export_base_path = os.path.expanduser('~/Desktop/projects/tfserving/serve_models9_tensor')
+        model_weights = '/Volumes/Elements/output_finetune/ResNetV1_101/20190719_132123/ckpt/model-25000'
+        img_path = os.path.expanduser('~/Desktop/fire_002626.jpg')
+    elif os_type == 'Windows':
+        export_base_path = r'D:/projects/tfserving/serve_models9/'
+        # model_weights = r'F:/output_finetune/ResNetV1_101/20190708_135032/ckpt/model-2000'
+        # model_weights = r'F:\output_finetune\ResNetV1_101\20190710_124127\ckpt\model-2000'
+        model_weights = r'F:\output_finetune\ResNetV1_101\20190719_132123\ckpt\model-25000'
+        img_path = r'D:\data\baokong2\normal\27.jpg'
+    else:
+        raise ValueError('os_type not supported')
     export_version = '1'
-    input_type = InputType.BASE64_JPEG
-    # input_type = InputType.TENSOR
-    # model_weights = r'F:/output_finetune/ResNetV1_101/20190708_135032/ckpt/model-2000'
-    # model_weights = r'F:\output_finetune\ResNetV1_101\20190710_124127\ckpt\model-2000'
-    model_weights = r'F:\output_finetune\ResNetV1_101\20190719_132123\ckpt\model-25000'
+    # input_type = InputType.BASE64_JPEG
+    input_type = InputType.TENSOR
 
     export_path = os.path.join(export_base_path, export_version)
 
@@ -75,7 +86,7 @@ if __name__ == "__main__":
                     sess=sess,
                     input_type=input_type)
 
-        # test_model(model, r'D:\data\baokong2\normal\27.jpg', sess)
+        # test_model(model, img_path, sess)
 
         builder = saved_model_builder.SavedModelBuilder(export_path)
 
