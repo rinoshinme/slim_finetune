5c059c9691e71ac5ab789c5ba7953e921e7ab008
rinoshinme <rinoshinme@163.com>
2019-08-02 15:02:42 +0800

    efficientnet modification

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
 import os
+from tensorflow.python import pywrap_tensorflow
+import numpy as np
 from config import cfg
+# from models.model_utils import load_initial_weights
 import nets.efficientnet.efficientnet_builder as model_builder
+
+# DEFAULT_TRAIN_LAYERS = ['blocks15', 'head']
+DEFAULT_TRAIN_LAYERS = ['head']
 
 
 class EfficientNet(object):
@@ -15,12 +21,18 @@ class EfficientNet(object):
         else:
             self.WEIGHTS_PATH = weights_path
 
-        self.train_layers = train_layers
+        if train_layers == 'DEFAULT':
+            self.train_layers = DEFAULT_TRAIN_LAYERS
+        else:
+            self.train_layers = train_layers
 
         with tf.variable_scope('inputs'):
-            self.inputs = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 3], name='inputs')
-            self.normalized_inputs = self.normalize_features(self.inputs, model_builder.MEAN_RGB, model_builder.STDDEV_RGB)
-            self.labels = tf.placeholder(tf.float32, [None, self.num_classes], name='labels')
+            # self.inputs = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 3], name='inputs')
+            # self.normalized_inputs = self.normalize_features(self.inputs,
+            #                                                  model_builder.MEAN_RGB,
+            #                                                  model_builder.STDDEV_RGB)
+            self.x_input = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 3], name='inputs')
+            self.y_input = tf.placeholder(tf.float32, [None, self.num_classes], name='labels')
             self.learning_rate = tf.placeholder(tf.float32, name="learning_rate")
             self.keep_prob = None
 
@@ -28,7 +40,8 @@ class EfficientNet(object):
 
         with tf.variable_scope('efficientnet', reuse=tf.AUTO_REUSE):
             self.logits, _ = model_builder.build_model(
-                self.normalized_inputs,
+                # self.normalized_inputs,
+                self.x_input,
                 model_name=self.model_name,
                 training=True,
                 override_params=additional_params,
@@ -36,7 +49,8 @@ class EfficientNet(object):
             )
 
             self.logits_val, _ = model_builder.build_model(
-                self.normalized_inputs,
+                # self.normalized_inputs,
+                self.x_input,
                 model_name=self.model_name,
                 training=False,
                 override_params=additional_params,
@@ -45,9 +59,9 @@ class EfficientNet(object):
 
         with tf.name_scope('loss'):
             self.loss = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.labels))
+                tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y_input))
             self.loss_val = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits_val, labels=self.labels))
+                tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits_val, labels=self.y_input))
 
         with tf.name_scope("train"):
             self.global_step = tf.Variable(0, name="global_step", trainable=False)
@@ -78,7 +92,7 @@ class EfficientNet(object):
             self.prediction = tf.argmax(self.logits_val, 1, name="prediction")
 
         with tf.name_scope("accuracy"):
-            correct_prediction = tf.equal(self.prediction, tf.argmax(self.labels, 1))
+            correct_prediction = tf.equal(self.prediction, tf.argmax(self.y_input, 1))
             self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name="accuracy")
 
     @staticmethod
@@ -89,9 +103,45 @@ class EfficientNet(object):
         features /= tf.constant(stddev_rgb, shape=stats_shape, dtype=features.dtype)
         return features
 
-    def load_weights(self, session):
-        session.run(tf.global_variables_initializer())
-        # load_initial_weights(self.session, weight_path, train_layers=[])
-        session.run(tf.global_variables_initializer())
-        saver = tf.train.Saver(var_list=tf.global_variables())
-        saver.restore(session, self.WEIGHTS_PATH)
+    def load_initial_weights(self, session, weight_path=None, train_layers=None):
+        # session.run(tf.global_variables_initializer())
+        # saver = tf.train.Saver(var_list=tf.global_variables())
+        # saver.restore(session, self.WEIGHTS_PATH)
+        if weight_path is None:
+            weight_path = self.WEIGHTS_PATH
+        if train_layers is None:
+            train_layers = self.train_layers
+
+        # load_initial_weights(session, weight_path, train_layers)
+
+        # do efficientnet specific weight loading
+        reader = pywrap_tensorflow.NewCheckpointReader(weight_path)
+        var_to_shape_map = reader.get_variable_to_shape_map()
+
+        for op_name in var_to_shape_map:
+            if op_name == 'global_step':
+                continue
+
+            # ignore exponential moving average.
+            if op_name.endswith('ExponentialMovingAverage'):
+                continue
+
+            op_name_list = op_name.split('/')
+            union_list = [item for item in op_name_list if item in train_layers]
+            if len(union_list) != 0:
+                continue
+
+            try:
+                with tf.variable_scope('efficientnet/' + '/'.join(op_name.split('/')[0:-1]), reuse=True):
+                    data = reader.get_tensor(op_name)
+                    var = tf.get_variable(op_name.split('/')[-1], trainable=False)
+                    # var = tf.get_variable(op_name.split('/')[-1], trainable=True)
+                    session.run(var.assign(data))
+            except ValueError:
+                tmp1 = list(op_name in str(item) for item in tf.global_variables())
+                tmp2 = np.sum([int(item) for item in tmp1])
+                if tmp2 == 0:
+                    print("Don't be loaded: {}, cause: {}".format(op_name, "new model no need this variable."))
+                else:
+                    print("Don't be loaded: {}, cause: {}".format(op_name, ValueError))
+        print('Loading parameters finished...')
